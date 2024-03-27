d05f92d936f1c132051566c59572aed4aa434e32
Ricardo Chino <ricardoychino@gmail.com>
2024-03-27 16:19:45 -0300

    Refactor

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,11 @@ const items = reactive(pricing)
 </script>
 
 <template>
-  <main class="w-[100vw] min-h-[100vh] py-10 bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-slate-100">
+  <main class="w-full min-h-screen py-10 bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-slate-100">
     <div class="mb-8 px-4 text-right">
       <ToggleButton />
     </div>
-    <div class="flex flex-wrap justify-center gap-5 m-auto">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 px-6 m-auto max-w-full md:max-w-[1000px]">
       <PricingCard v-for="item in items"
         :name="item.name"
         :price="item.price"
--- src/components/PricingCard.vue
+++ src/components/PricingCard.vue
@@ -15,14 +15,14 @@ defineProps({
 </script>
 
 <template>
-  <div class="prose flex flex-col rounded-md box-border shadow-lg p-4 basis-[300px] h-auto border bg-white border-slate-50 dark:bg-slate-700 dark:text-slate-100 dark:border-slate-600">
-    <h2 class="text-center dark:text-white">{{ name }}</h2>
-    <p class="text-center font-light text-gray-400 dark:text-gray-500">{{ description }}</p>
-    <div class="text-center my-2">
+  <div class="prose flex flex-col rounded-md box-border text-center shadow-lg p-4 h-auto border bg-white border-slate-50 dark:bg-slate-700 dark:text-slate-100 dark:border-slate-600">
+    <h2 class="dark:text-white">{{ name }}</h2>
+    <p class="font-light text-gray-400 dark:text-gray-500">{{ description }}</p>
+    <div class="my-2">
       <span class="font-bold text-4xl">${{ price }}</span>
       <span class="ml-2 text-md text-gray-400 dark:text-gray-500">/month</span>
     </div>
-    <ul class="mb-8 p-0">
+    <ul class="mb-8 p-0 text-left">
       <li v-for="feature in features"
         :key="`${name}-${feature}`"
         class="flex items-center text-sm mb-3"
