9cf3d6230c89d6966b6cea996751fd7352812ee0
Ricardo Chino <ricardoychino@gmail.com>
2024-03-27 13:37:21 -0300

    Base components and data

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script setup>
+import { reactive } from "vue";
+import { pricing } from "@/pricing.json"
+import PricingCard from "@/components/PricingCard.vue"
+
+const items = reactive(pricing)
 </script>
 
 <template>
-  <main>
-    aa
+  <main class="flex justify-center items-center gap-5 py-10 w-[100vw] h-[100vh]">
+    <PricingCard v-for="item in items"
+      :name="item.name"
+      :price="item.price"
+      :description="item.description"
+      :features="item.features"
+      :key="item.name.toLowerCase()"
+    />
   </main>
 </template>
---
+++ src/components/PricingCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  price: Number,
+  description: String,
+  features: {
+    default: []
+  }
+})
+</script>
+
+<template>
+  <div class="prose shadow-md">
+    <h2>{{ name }}</h2>
+    <p>{{ description }}</p>
+    <div>
+      <span>${{ price }}</span>
+      <span>/month</span>
+    </div>
+    <ul>
+      <li v-for="feature in features" :key="`${name}-${feature}`">
+        {{ feature }}
+      </li>
+    </ul>
+    <button>Get started</button>
+  </div>
+</template>
