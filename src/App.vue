88ca8cff0b82170b1f6a77e915a42ec1c36d6309
Ricardo Chino <ricardoychino@gmail.com>
2024-03-27 15:30:38 -0300

    Dark mode toggle button added

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
 import { reactive } from "vue";
 import { pricing } from "@/pricing.json"
 import PricingCard from "@/components/PricingCard.vue"
+import ToggleButton from "@/components/ToggleButton.vue"
+
 
 const items = reactive(pricing)
 </script>
 
 <template>
-  <main class="w-[100vw] min-h-[100vh] py-10 bg-slate-100">
-    <div class="mb-8">
-      <button>
-        Toggle mode
-      </button>
+  <main class="w-[100vw] min-h-[100vh] py-10 bg-slate-100 text-slate-900">
+    <div class="mb-8 px-4 text-right">
+      <ToggleButton />
     </div>
     <div class="flex flex-wrap justify-center gap-5 m-auto">
       <PricingCard v-for="item in items"
---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+import { SunIcon, MoonIcon } from "@heroicons/vue/24/solid"
+
+const isDarkMode = useDark()
+const toggleDark = useToggle(isDarkMode)
+</script>
+
+<template>
+  <button 
+    class="p-1 rounded-md bg-slate-300 text-slate-800"
+    @click="toggleDark()"><!-- Needs to include () - source: https://vueuse.org/shared/useToggle/#usetoggle -->
+    <SunIcon class="toggle-icon" v-show="isDarkMode" />
+    <MoonIcon class="toggle-icon" v-show="!isDarkMode" />
+  </button>
+</template>
+
+<style scoped>
+.toggle-icon {
+  @apply h-7 w-7;
+}
+</style>
